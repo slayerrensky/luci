8e587d141e1edbe691bda261dbafda6aee151735
slayerrensky <rensky.g@googlemail.com>
2015-04-03 22:34:54 +0200

    add bebox

---
+++ applications/luci-app-beschuetzerbox/luasrc/controller/beschuetzerbox/bbcontroller.lua
@@ -0,0 +1,86 @@
+module("luci.controller.beschuetzerbox.bbcontroller", package.seeall)  --notice that new_tab is the name of the file new_tab.lua
+ 
+function index()
+
+        local uci = require "luci.model.uci".cursor()
+        local page
+
+        -- Frontend
+        page          = node()
+        page.lock     = true
+        page.target   = alias("bebox")
+        page.subindex = true
+        page.index    = false
+
+        page          = node("bebox")
+        page.title    = _("Beschuetzerbox")
+        page.target   = alias("bebox", "index")
+        page.order    = 5
+        page.setuser  = "nobody"
+        page.setgroup = "nogroup"
+        page.i18n     = "freifunk"
+        page.index    = true
+
+        page          = node("bebox", "index")
+        page.target   = template("beschuetzerbox/index")
+        page.title    = _("Overview")
+        page.order    = 10
+        page.indexignore = true
+
+        page          = node("bebox", "wireless")
+        page.target   = template("beschuetzerbox/config")
+        page.title    = _("Wireless")
+        page.order    = 20
+        page.i18n     = "base"
+        page.setuser  = false
+        page.setgroup = false
+
+	page = entry({"bebox", "wireless", "commit_wireless"}, call("commit_wireless"), nil)
+        page.leaf = true
+
+        entry({"bebox", "index", "zeroes"}, call("zeroes"), "Testdownload")
+	entry({"bebox", "wireless", "wireless_set"}, template("beschuetzerbox/wireless_set"),"Config Wireless", 50)
+	
+	-- Backand
+	entry({"admin", "bebox"}, firstchild(), "Beschuetzerbox", 30).dependent=false
+	entry({"admin", "bebox", "config"}, cbi("beschuetzerbox/config"), "Config", 1)
+
+end
+
+
+function zeroes()
+        local string = require "string"
+        local http = require "luci.http"
+        local zeroes = string.rep(string.char(0), 8192)
+        local cnt = 0
+        local lim = 1024 * 1024 * 1024
+
+        http.prepare_content("application/x-many-zeroes")
+
+        while cnt < lim do
+                http.write(zeroes)
+                cnt = cnt + #zeroes
+        end
+end
+
+function commit_wireless(ssid, key, password)
+        luci.http.prepare_content("text/plain")
+        a = string.format("wifiuserset.sh %s %s %s",ssid, key, password)
+        local util = io.popen(a)
+        if util then
+                while true do
+                        local ln = util:read("*l")
+                        if not ln then break end
+                        -- luci.http.write(a)
+                        luci.http.write(ln)
+                        luci.http.write("\n")
+                end
+
+                util:close()
+        end
+        -- luci.http.write(a)
+        -- luci.http.write("\n")
+
+end
+
+
---
+++ applications/luci-app-beschuetzerbox/root/usr/sbin/wifiuserset.sh
@@ -0,0 +1,45 @@
+#!/bin/ash
+
+# This script set SSID and Passwort from a particular Wifi interface
+# with is defined for costumer user.
+# created by Ren.. Galow, rensky at gmx.de
+
+INTERFACE=$(uci show wireless | grep setable | sed 's/.*\[\(.*\)\][^"]*$/\1/')
+SSID=$1
+KEY=$2
+PASSWORD=$3
+UCIPASSWORD=$(uci get bebox.UserConfig.password)
+
+if [ x$SSID = x ]; then
+    echo "USAGE: $0 SSID KEY PASSWORD"
+    exit 1
+fi
+
+
+if [ $KEY != "null" ]; then
+  if [ x$KEY = x ];  then
+    echo "USAGE: $0 SSID KEY PASSWORD"
+    exit 1
+  fi
+  KEYLENGHT=$(echo ${#KEY})
+  if [ $KEYLENGHT -gt 64 ] || [ $KEYLENGHT -lt 8 ]; then
+      echo "Key lenght must be between 8 an 64 character."
+      exit 1
+  fi
+fi
+
+
+if [ "$PASSWORD" != "$UCIPASSWORD" ]; then
+    echo "Password not match."
+    exit 1
+fi
+
+if [ $KEY == "null" ]; then
+
+else
+  uci set wireless.@wifi-iface[$INTERFACE].ssid=$SSID
+  uci set wireless.@wifi-iface[$INTERFACE].key=$KEY
+fi
+uci commit wireless
+wifi
+
